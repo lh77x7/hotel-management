b7e4b83f448a9ada07e03a34a418978ada551827
unknown <>
2020-12-27 16:06:30 +0100

    establish module and modules planning

--- hotel-management.py
+++ hotel-management.py
@@ -30,4 +30,33 @@ def mysqlConnectionCheck():
     else:
         print("Check username and password!")
 
-mysqlConnectionCheck()
+#mysql connection established module
+def mysqlConnection():
+    global username
+    global password
+    global myConnection
+    global cid
+    myConnection=mysql.connector.connect(host="localhost", user=userName, password=password)
+    if myConnection:
+        return myConnection
+    else:
+        print("\nMysql connection failed")
+        myConnection.close()
+
+#new user input deta module
+
+#booking input date module
+
+#rooms input rent module
+
+#gaming input module
+
+#fashion input module
+
+#totalAmount module
+
+#searching older bill module
+
+#searching customer module
+
+#main menu module
